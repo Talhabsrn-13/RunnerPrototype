5d25ae14f34a3d8eb5d162eb4ae06db69c7d6b83
TalhaBaşaran <tbsrn7@gmail.com>
2023-02-02 02:44:43 +0300

    Floor controller script
    Floor script floor prefabine verildi o da variantlarına iletti. Platform da haraket ediyormuş gibi bi görüntü oluşturuldu.

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/FloorController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunnerPrototype2.Controllers
+{
+    public class FloorController : MonoBehaviour
+    {
+        Material _material;
+        [Range(0.5f,2)]
+        [SerializeField] float _moveSpeed;
+
+        private void Awake()
+        {
+            _material = GetComponentInChildren<MeshRenderer>().material;
+        }
+        private void Update()
+        {
+            _material.mainTextureOffset += Vector2.down * Time.deltaTime * _moveSpeed;
+        }
+    }
+}
+
