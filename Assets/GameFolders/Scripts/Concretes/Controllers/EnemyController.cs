c58fbe172183d48721e20669027034916f2d5e70
TalhaBaşaran <tbsrn7@gmail.com>
2023-02-02 17:36:23 +0300

    Enemy  script
    enemy script oluşturuldu spawner prefableri oluşturuldu fakat scripti yazılmadı

--- Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -8,14 +8,31 @@ namespace RunnerPrototype2.Controllers
     {
         VerticalMover _verticalMover;
         [SerializeField] float _moveSpeed;
+
+        [SerializeField] float _maxLifeTime = 7;
+        float _currentLifeTime;
         public float MoveSpeed => _moveSpeed;
         private void Awake()
         {
             _verticalMover = new VerticalMover(this);
         }
+        private void Update()
+        {
+            _currentLifeTime += Time.deltaTime;
+            if (_currentLifeTime > _maxLifeTime)
+            {
+                _currentLifeTime = 0;
+                KillYourSelf();
+            }
+        }
         private void FixedUpdate()
         {
             _verticalMover.FixedTick();
+        }
+
+        void KillYourSelf()
+        {
+            Destroy(this.gameObject);
         }
     }
 }
